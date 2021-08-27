a7164eb9f3789ddb392d009fdb8c17219be7b911
aslammamud <aslammamud@gmail.com>
2021-08-27 21:23:49 +0600

    fixed bug

--- app/src/main/java/com/aslam/androidcrudphp/views/HomeActivity.java
+++ app/src/main/java/com/aslam/androidcrudphp/views/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.aslam.androidcrudphp.views;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
@@ -9,6 +10,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -95,98 +97,24 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        //mAuth.signOut();
-        finishAffinity();
-        finish();
-    }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
 
-}
-
-/*
-
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.aslam.androidcrudphp.R;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.firebase.auth.FirebaseAuth;
-
-public class HomeActivity extends AppCompatActivity {
-
-    private Button sign_out_button, getInfo;
-    private TextView user_name;
-    //private GoogleSignInClient mGoogleSignInClient;
-    FirebaseAuth mAuth;
-
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        user_name = findViewById(R.id.user_name);
-        getInfo = findViewById(R.id.getUserInfo);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        sign_out_button = findViewById(R.id.sign_out_button);
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
-            Uri personPhoto = acct.getPhotoUrl();
-
-            user_name.setText("Welcome "+personName);
-
-        }
-
-*/
-/*        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);*//*
-
-
-        getInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(HomeActivity.this,acct.getDisplayName()+"   "+acct.getGivenName()+"   "+acct.getEmail()+"   "+acct.getId() ,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        sign_out_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-*/
-/*                mGoogleSignInClient.signOut()
-                        .addOnCompleteListener(HomeActivity.this, new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(getApplicationContext(),"Signing Out...",Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        });*//*
-
-
-                mAuth.signOut();
-                //finish();
-            }
-        });
+                        finishAffinity();
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
 
     }
 
-
 }
-*/
